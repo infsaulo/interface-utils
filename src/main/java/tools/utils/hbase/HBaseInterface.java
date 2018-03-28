d800b68b1c0390c2684fd7dee5ed6891f09f580a
infsaulo <infsaulok@gmail.com>
2018-03-28 01:34:10 -0700

    Adding getAll method to HBaseInterface

--- src/main/java/tools/utils/hbase/HBaseInterface.java
+++ src/main/java/tools/utils/hbase/HBaseInterface.java
@@ -3,11 +3,7 @@ package tools.utils.hbase;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
 import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.filter.RegexStringComparator;
@@ -21,134 +17,151 @@ import java.util.logging.Logger;
 
 public class HBaseInterface {
 
-  private static final Logger LOGGER = Logger.getLogger(HBaseInterface.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(HBaseInterface.class.getName());
 
 
-  public static ResultScanner searchByRegex(Table table, String regex) {
+    public static ResultScanner searchByRegex(Table table, String regex) {
 
-    final RegexStringComparator regexComp = new RegexStringComparator(regex);
+        final RegexStringComparator regexComp = new RegexStringComparator(regex);
 
-    final RowFilter filter = new RowFilter(CompareOp.EQUAL, regexComp);
+        final RowFilter filter = new RowFilter(CompareOp.EQUAL, regexComp);
 
-    final Scan rowScan = new Scan();
+        final Scan rowScan = new Scan();
 
-    rowScan.setFilter(filter);
+        rowScan.setFilter(filter);
 
-    final ResultScanner results;
+        final ResultScanner results;
 
-    try {
+        try {
 
-      results = table.getScanner(rowScan);
+            results = table.getScanner(rowScan);
 
-      return results;
-    } catch (IOException exception) {
+            return results;
+        } catch (IOException exception) {
 
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
-      return null;
-    }
-  }
-
-  public static ResultScanner searchByPrefix(Table table, String prefix) {
-
-    final PrefixFilter prefixFilter = new PrefixFilter(Bytes.toBytes(prefix));
-
-    final Scan prefixScan = new Scan();
-
-    prefixScan.setFilter(prefixFilter);
-
-    final ResultScanner results;
-
-    try {
-
-      results = table.getScanner(prefixScan);
-
-      return results;
-    } catch (IOException exception) {
-
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
-      return null;
-    }
-  }
-
-  public static ResultScanner searchByRange(final Table table, final String fromRow,
-                                            final String toRow) {
-
-    final Scan rangeScan = new Scan(Bytes.toBytes(fromRow), Bytes.toBytes(toRow));
-
-    final ResultScanner results;
-    try {
-
-      results = table.getScanner(rangeScan);
-
-      return results;
-    } catch (IOException exception) {
-
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
-      return null;
-    }
-  }
-
-  public static void closeResultScanner(ResultScanner results) {
-
-    results.close();
-  }
-
-  public static Connection getConnection(final Map<String, String> configParams) {
-
-    final Configuration conf = HBaseConfiguration.create();
-
-    for (Map.Entry<String, String> param : configParams.entrySet()) {
-
-      conf.set(param.getKey(), param.getValue());
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+            return null;
+        }
     }
 
-    try {
+    public static ResultScanner searchByPrefix(Table table, String prefix) {
 
-      final Connection conn = ConnectionFactory.createConnection(conf);
+        final PrefixFilter prefixFilter = new PrefixFilter(Bytes.toBytes(prefix));
 
-      return conn;
-    } catch (IOException exception) {
+        final Scan prefixScan = new Scan();
 
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
+        prefixScan.setFilter(prefixFilter);
 
-      return null;
+        final ResultScanner results;
+
+        try {
+
+            results = table.getScanner(prefixScan);
+
+            return results;
+        } catch (IOException exception) {
+
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+            return null;
+        }
     }
-  }
 
-  public static Table getTable(final Connection conn, final String tableName) {
+    public static ResultScanner searchByRange(final Table table, final String fromRow,
+                                              final String toRow) {
 
-    try {
+        final Scan rangeScan = new Scan(Bytes.toBytes(fromRow), Bytes.toBytes(toRow));
 
-      final Table table = conn.getTable(TableName.valueOf(tableName));
+        final ResultScanner results;
+        try {
 
-      return table;
-    } catch (IOException exception) {
+            results = table.getScanner(rangeScan);
 
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
+            return results;
+        } catch (IOException exception) {
 
-      return null;
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+            return null;
+        }
     }
-  }
 
-  public static void closeTable(final Table table) {
+    public static ResultScanner getAll(final Table table) {
 
-    try {
+        final Scan scan = new Scan();
 
-      table.close();
-    } catch (IOException exception) {
+        final ResultScanner results;
+        try {
 
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
+            results = table.getScanner(scan);
+
+            return results;
+        } catch (IOException exception) {
+
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+            return null;
+        }
     }
-  }
 
-  public static void closeConnection(final Connection conn) {
+    public static void closeResultScanner(ResultScanner results) {
 
-    try {
-
-      conn.close();
-    } catch (IOException exception) {
-
-      LOGGER.log(Level.SEVERE, exception.toString(), exception);
+        results.close();
     }
-  }
+
+    public static Connection getConnection(final Map<String, String> configParams) {
+
+        final Configuration conf = HBaseConfiguration.create();
+
+        for (Map.Entry<String, String> param : configParams.entrySet()) {
+
+            conf.set(param.getKey(), param.getValue());
+        }
+
+        try {
+
+            final Connection conn = ConnectionFactory.createConnection(conf);
+
+            return conn;
+        } catch (IOException exception) {
+
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+
+            return null;
+        }
+    }
+
+    public static Table getTable(final Connection conn, final String tableName) {
+
+        try {
+
+            final Table table = conn.getTable(TableName.valueOf(tableName));
+
+            return table;
+        } catch (IOException exception) {
+
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+
+            return null;
+        }
+    }
+
+    public static void closeTable(final Table table) {
+
+        try {
+
+            table.close();
+        } catch (IOException exception) {
+
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+        }
+    }
+
+    public static void closeConnection(final Connection conn) {
+
+        try {
+
+            conn.close();
+        } catch (IOException exception) {
+
+            LOGGER.log(Level.SEVERE, exception.toString(), exception);
+        }
+    }
 }
